71288c589e8fbecf662b37101b556fb3bb8d12ff
Lungu Marina-Diana <marinadiana1999@yahoo.com>
2024-05-08 20:34:11 +0300

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,40 @@ import PauseIcon from '@mui/icons-material/Pause';
 import CasinoIcon from '@mui/icons-material/Casino';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useState, useEffect } from 'react';
 import FavoriteAdvicesModal from './components/FavoriteAdvicesModal/FavoriteAdvicesModal';
-
+import { useState, useEffect } from 'react';
+import { getCurrentDateFormatted } from './utils/getCurrentDateFormatted';
 
 const App = () => {
-  const[advice, setadvice] = useState({
-    id: 117,
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consectetur! Magnam necessitatibus similique tempora temporibus',
-  });
-
+  const [advice, setAdvice] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const [favoriteAdvices, setFavoriteAdvices] = useState ([]);
 
+  const favoriteAdvicesFromLocalStorage = JSON.parse(
+    localStorage.getItem('favoritedAdvicesLS'),
+  );
+
+  const [favoriteAdvices, setFavoriteAdvices] = useState(
+    favoriteAdvicesFromLocalStorage === null
+      ? []
+      : favoriteAdvicesFromLocalStorage,
+  );
+
+  const getInitialAdvice = async () => {
+    const serverResponse = await fetch('https://api.adviceslip.com/advice');
+    const data = await serverResponse.json();
+
+    setAdvice({
+      id: data.slip.id,
+      content: data.slip.advice,
+    });
+  };
 
   useEffect(() => {
-    const storedAdvices = JSON.parse(localStorage.getItem('favoriteAdvices'));
-    if (storedAdvices) {
-      setFavoriteAdvices(storedAdvices);
-    }
-  }, []); 
+    getInitialAdvice();
+  }, []);
 
-
-  const handleGenerateAdvice = async() => {
+  const handleGenerateAdvice = async () => {
     setIsLoading(true);
 
     const serverResponse = await fetch('https://api.adviceslip.com/advice');
@@ -38,92 +48,119 @@ const App = () => {
       id: data.slip.id,
       content: data.slip.advice,
     };
-    setadvice(newAdvice);
-  };
 
+    setAdvice(newAdvice);
+  };
 
   const getAdviceIndex = () => {
-    const adviceIndex = favoriteAdvices.findIndex((favoriteAdvice) => favoriteAdvice.id === advice.id);
+    const adviceIndex = favoriteAdvices.findIndex(
+      favoriteAdvice => favoriteAdvice.id === advice?.id,
+    );
+
     return adviceIndex;
   };
-
-
-  const isAdviceInFavorites = () => {
-    return favoriteAdvices.some((favoriteAdvice) => favoriteAdvice.id === advice.id);
-  };
-
-
-  const handleDeletedAdvice = (adviceId) => {
-    const updatedFavoriteAdvices = favoriteAdvices.filter(favoriteAdvice => favoriteAdvice.id !== adviceId);
-    setFavoriteAdvices(updatedFavoriteAdvices);
-
-    localStorage.setItem('favoriteAdvices', JSON.stringify(updatedFavoriteAdvices));
-  };
-
 
   const handleAddAdviceToFavorites = () => {
     const adviceIndex = getAdviceIndex();
 
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-    const dateAddedAdvice = `${day}/${month + 1}/${year}`;
-
     if (adviceIndex >= 0) {
       // elimina advice
       const newFavoriteAdvices = [...favoriteAdvices];
-      newFavoriteAdvices.splice(adviceIndex, 1); 
+      newFavoriteAdvices.splice(adviceIndex, 1);
 
       setFavoriteAdvices(newFavoriteAdvices);
+      localStorage.setItem(
+        'favoritedAdvicesLS',
+        JSON.stringify(newFavoriteAdvices),
+      );
     } else {
       // adauga advice
-      const newFavoriteAdvices = [...favoriteAdvices, {
-        id: advice.id, 
-        content: advice.content,
-        date: dateAddedAdvice,
+      const newFavoriteAdvices = [
+        ...favoriteAdvices,
+        {
+          id: advice.id,
+          content: advice.content,
+          addedAt: getCurrentDateFormatted(),
         },
       ];
-      setFavoriteAdvices(newFavoriteAdvices);
 
-      localStorage.setItem('favoriteAdvices', JSON.stringify(newFavoriteAdvices));
+      setFavoriteAdvices(newFavoriteAdvices);
+      localStorage.setItem(
+        'favoritedAdvicesLS',
+        JSON.stringify(newFavoriteAdvices),
+      );
     }
   };
 
+  const removeAdviceFromFavorites = adviceId => {
+    // gaseste index-ul advice-ului al carui id il primim ca parametru
+    const adviceIndexInsideFavorites = favoriteAdvices.findIndex(
+      favoriteAdvice => favoriteAdvice.id === adviceId,
+    );
+
+    const newFavoriteAdvices = [...favoriteAdvices];
+
+    newFavoriteAdvices.splice(adviceIndexInsideFavorites, 1);
+
+    setFavoriteAdvices(newFavoriteAdvices);
+  };
 
   const handleModalOpening = () => {
     setIsOpen(true);
   };
 
-  
   const closeModal = () => {
     setIsOpen(false);
   };
 
-
   return (
-    <section className='app-container'>
-      <button onClick={handleModalOpening} className='show-favorites'> Show favorites </button>
-      
-      {isOpen === true ?  (<FavoriteAdvicesModal  advices={favoriteAdvices} deleteAdvice={handleDeletedAdvice} setIsOpen={closeModal} />) : null}
+    <section className="app-container">
+      <button onClick={handleModalOpening} className="show-favorites">
+        Show favorites
+      </button>
 
-      <div className='advice-slip-container'>
-        <button onClick = {handleAddAdviceToFavorites} className='toggle-favorite-button'> 
-          {
-            isAdviceInFavorites() ? (<FavoriteIcon style={{color: '#52ffa8'}} />) : (<FavoriteBorderIcon style={{color: '#52ffa8'}} />) 
-          }
+      {isOpen ? (
+        <FavoriteAdvicesModal
+          advices={favoriteAdvices}
+          setIsOpen={closeModal}
+          removeAdvice={removeAdviceFromFavorites}
+        />
+      ) : null}
+
+      <div className="advice-slip-container">
+        <button
+          onClick={handleAddAdviceToFavorites}
+          className="toggle-favorite-button"
+        >
+          {getAdviceIndex() === -1 ? (
+            <FavoriteBorderIcon style={{ color: '#52ffa8' }} />
+          ) : (
+            <FavoriteIcon style={{ color: '#52ffa8' }} />
+          )}
         </button>
-        <p className='advice-id'> ADVICE #{advice.id} </p>
-        <p className='advice-content'> "{advice.content}" </p>
-        <div className='separator-container'>
+        {advice !== null ? (
+          <>
+            <p className="advice-id"> ADVICE #{advice.id} </p>
+            <p className="advice-content"> "{advice.content}" </p>
+          </>
+        ) : (
+          <p className="advice-content advice-content-spinner"> Loading... </p>
+        )}
+        <div className="separator-container">
           <hr />
-            <PauseIcon style={{color: '#cee3e9'}} />
+          <PauseIcon style={{ color: '#cee3e9' }} />
           <hr />
         </div>
-        <button onClick={handleGenerateAdvice} className='advice-button' disabled = {isLoading === true ? true : false}>
-          {
-            isLoading === true ? (<div className="spinner"></div>) : (<CasinoIcon fontSize={'large'} />)
-          }  
+        <button
+          onClick={handleGenerateAdvice}
+          className="advice-button"
+          disabled={isLoading === true ? true : false}
+        >
+          {isLoading === true ? (
+            <div className="spinner"></div>
+          ) : (
+            <CasinoIcon fontSize={'large'} />
+          )}
         </button>
       </div>
     </section>
--- src/components/FavoriteAdvicesModal/FavoriteAdvicesModal.jsx
+++ src/components/FavoriteAdvicesModal/FavoriteAdvicesModal.jsx
@@ -1,18 +1,36 @@
-import './FavoriteAdvicesModal.css'
+import FavoriteAdvice from '../FavoriteAdvice/FavoriteAdvice';
+import './FavoriteAdvicesModal.css';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
-import FavoriteAdvicesContainer from '../FavoriteAdvicesContainer/FavoriteAdvicesContainer';
 
-const FavoriteAdvicesModal = (props) => {
-  return ( 
+const FavoriteAdvicesModal = props => {
+  return (
     <>
       <div onClick={props.setIsOpen} className="overlay"></div>
       <div className="favorite-advices-container favorite-advices-scrollbar">
         <div className="favorite-advices-title-container">
           <h2 className="favorite-advice-title"> My favorite Advices </h2>
-          <AutoAwesomeIcon style={{color: '#4e5d73'}} stroke={"#1f2632"} strokeWidth={1} />
+          <AutoAwesomeIcon
+            style={{ color: '#4e5d73' }}
+            stroke={'#1f2632'}
+            strokeWidth={0.8}
+          />
         </div>
-        {props.advices.length === 0 ? (<p className="favorite-advice-message"> No favorite advices added yet </p>) : (<FavoriteAdvicesContainer advices={props.advices} deleteAdvice={props.deleteAdvice} />)}
-    </div>
+        <div>
+          {props.advices.length === 0 ? (
+            <p className="favorite-advice-message">
+              No favorite advices added yet
+            </p>
+          ) : (
+            props.advices.map(advice => (
+              <FavoriteAdvice
+                key={advice.id}
+                advice={advice}
+                removeAdvice={props.removeAdvice}
+              />
+            ))
+          )}
+        </div>
+      </div>
     </>
   );
 };
